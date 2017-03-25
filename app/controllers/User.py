2c4ab6faec30b741b2bef1ddc978ecf1b0cff92e
Guilherme <gresendesa@gmail.com>
2017-03-25 01:42:01 -0300

    Empty

--- app/controllers/User.py
+++
@@ -1,8 +0,0 @@
-from framework import Controller as default
-
-class User(default.Controller):
-	def __init__(self):
-		pass
-
-	def sample(self, variables = None):
-		return "hello bitch"
--- framework/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-framework package
-
-This package contains the main objects to serve the framework purposes. 
-It means processes not directly related to the app functionalities.
-"""
-
