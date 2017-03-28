1f46333216dbdcf212285dad10340bb714d54ed7
matiasvictor <matias.victor@hotmail.com>
2017-03-28 16:00:08 -0300

    Script simples de conexão com o banco de dados

---
+++ app/db/scripts/importmysql.py
@@ -0,0 +1,20 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="pibic",  # your password
+                     db="Uniwallet")        # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor()
+
+# Use all the SQL you like
+cur.execute("SELECT * FROM Users")
+
+# print all the first cell of all the rows
+for row in cur.fetchall():
+    print row[0]
+    print row[1]
+
+db.close()
