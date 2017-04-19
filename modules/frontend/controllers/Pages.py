f7d4d4f597d901a5c85a3cc07665ebb1946f19a6
Guilherme Sá <gresendesa@gmail.com>
2017-04-19 09:13:48 -0300

    TeladeCadastro.html renderized

---
+++ modules/frontend/controllers/Pages.py
@@ -0,0 +1,11 @@
+from framework import Controller as std
+
+class Pages(std.Controller):
+
+	def cadastro(self):
+		host = "http://{}:{}".format(self.request.env['SERVER_NAME'], self.request.env['SERVER_PORT'])
+		html = self.get_html("TeladeCadastro")
+		html = html.replace("{assets-js}", "/frontend/static/js?target=").replace("{assets-css}", "/frontend/static/css?target=").replace("{assets-img}", "/frontend/static/img?target=")
+		return html
+
+
