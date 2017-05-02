7f4064cab7cbc415d9fde066d817cfeb94adaf77
redes <aluno@redes-00@example.org>
2017-05-02 12:06:37 -0300

    Funçao Support por Email

---
+++ modules/backend/controllers/treaters/Support.py
@@ -0,0 +1,13 @@
+from framework import Treater as std
+
+class Support(std.Treater):
+	
+	def report(self):
+		return self.rules({
+				"fields": {
+					"email": 	["required", "email"],
+					"name": 	["required"],
+					"content": 	["required"]
+				},
+				"method": "get"
+			})
