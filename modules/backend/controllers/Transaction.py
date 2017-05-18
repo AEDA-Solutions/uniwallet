6a5f7f96bcb3aa0488e2a11ea00c838fbb3d42d2
= <guilherme-gomesc@hotmail.com>
2017-05-17 22:38:03 -0700

    Transaction changes

--- modules/backend/controllers/Transaction.py
+++ modules/backend/controllers/Transaction.py
@@ -2,11 +2,11 @@ from framework import Controller as std
 
 	class Transaction(std.Controller)
 
-		def sucess(self)
-		pass
+		def pending(self)
+		if self.model.find([('user_id'. '=', '')]).fetchone()
+			else return "Essa transacao e invalida"
 
-		def failure(self)
 
 		def save(self)
 		transaction_id = self.model(self.get_request_parameters()).save().last_id()
-		return "Done: Transaction {} created successfully".format(last_id)
+		return "Done: Transaction {} created successfully".format(transaction_id)
