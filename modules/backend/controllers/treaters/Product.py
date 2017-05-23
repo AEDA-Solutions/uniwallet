c4a5bf52bcaa61b911c1b428d89c67b45c4bb661
Arthur <arthurlenzi@gmail.com>
2017-05-23 10:29:13 -0300

    Updates

--- modules/backend/controllers/treaters/Product.py
+++ modules/backend/controllers/treaters/Product.py
@@ -4,12 +4,12 @@ class Product(std.Treater):
 	def register(self):
 		return self.rules({
 				"fields": {
+					"company_id": 	["required", "exists:id:Company"]
 					"number": 		["required"],
 					"name": 		["required"],
-					"price":		["required"],
 					"description":	["required"],
 					"category":		["required"],
-					"company_id": 	["required", "exists:id:Company"]
+					
 				},
 				"method": "post",
 				#"auth": ["manager", "client"]
