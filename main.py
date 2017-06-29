9f42f5d11261268fa3341f80ee5aa6eb3c9eb3b2
gresendesa <gresendesa@gmail.com>
2017-06-29 00:17:07 -0300

    main.py recovered

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+main
+
+This snippet is executed just once for the machine.
+But it creates a thread that keeps listening 'forever' for requests until it is quit. 
+It means it is just a spark for the system which is actually built through the boostrap process.
+"""
+
+from wsgiref.simple_server import make_server
+from framework.bootstrap import bootstrap
+	
+host = 'localhost'
+port = 8000
+
+httpd = make_server(host, port, bootstrap)
+print("Serving on port {}...".format(port))
+httpd.serve_forever()
